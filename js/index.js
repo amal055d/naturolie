63c24cab7a7b381928a538b238f701a1e33fe101
amal055d <amal055d@stud.kea.dk>
2021-03-23 13:45:12 +0100

    alt muligt lort

--- js/index.js
+++ js/index.js
@@ -3,14 +3,30 @@ gsap.registerPlugin(ScrollTrigger);
 const divs = document.querySelectorAll("#sektion1 > div");
 
 gsap.from(divs, {
-  opacity: 0,
   x: 100,
   duration: 3,
   stagger: 1,
   ScrollTrigger: {
     trigger: "#sektion1",
-    start: "top 50%",
-    end: "bottom 75%",
-    markers: true,
+    start: "top center",
   },
+});
+
+ScrollTrigger.create({
+  trigger: "divs",
+
+  toggleClass: "active",
+  //   toggleClass: { targets: "#produkt1 h1", className: "active" },
+  once: true,
+  markers: true,
+});
+
+const p = document.querySelector("p");
+
+ScrollTrigger.create({
+  trigger: p,
+  start: "top center",
+  toggleClass: "animate__headShake",
+  once: true,
+  markers: true,
 });
